aa3c791485b8952b1b6f9ca442321c3623815e32
Александр Александрович Чуносов <goldengooserider999@gmail.com>
2022-10-04 16:41:04 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace StupidCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                double firstValue, secondValue;
+                string action;
+
+                try
+                {
+                    Console.WriteLine("Enter first value: ");
+                    firstValue = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter second value: ");
+                    secondValue = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("It's not a number!");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                Console.WriteLine("Choose operation of calculate: ");
+                action = Console.ReadLine();
+
+                switch (action)
+                {
+                    case "+":
+                        Console.WriteLine("Result of + :" + (firstValue + secondValue));
+                        break;
+                    case "-":
+                        Console.WriteLine("Result of - :" + (firstValue - secondValue));
+                        break;
+                    case "*":
+                        Console.WriteLine("Result of * :" + (firstValue * secondValue));
+                        break;
+                    case "/":
+                        Console.WriteLine("Result of / :" + (firstValue / secondValue));
+                        break;
+                    default:
+                        Console.WriteLine("This is not operation");
+
+                        break;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
